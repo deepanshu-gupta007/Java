fc0874ddb3af7e5ef995455a949ab85faa2264a3
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-02-20 00:38:59 +0530

    Add files via upload

---
+++ P3/Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Conversion {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius: ");
+        float c=sc.nextFloat();
+        float f=1.8f*c+32;
+        System.out.println("Fahrenheit = "+f);
+    }
+}
