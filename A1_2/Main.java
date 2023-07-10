e4dc2b1c7b7dc8bf4601890367efa85c4b241449
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:15:00 +0530

    Add files via upload

--- A1_2/Main.java
+++ A1_2/Main.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+import java.text.*;
+import java.time.*;
+import java.time.format.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input a month number: ");
+        int m=sc.nextInt();
+        System.out.print("Input a year: ");
+        int y=sc.nextInt();
+        int d=YearMonth.of(y, m).lengthOfMonth();
+        String mon=Month.of(m).getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+        System.out.println(mon+" "+y+" has "+d+" days");
+        sc.close();
+    }
+}
