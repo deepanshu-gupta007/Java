96dc584b427a955fcb0307be3f3232b808e9bb1d
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:15:34 +0530

    Add files via upload

--- A1_3/Main.java
+++ A1_3/Main.java
@@ -1,1 +1,11 @@
-
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input number of terms: ");
+        int x=sc.nextInt();
+        for(int i=1; i<=x; i++)
+            System.out.println("Number is: "+i+" and cube of "+i+" is: "+(i*i*i));
+            sc.close();
+    }
+}
