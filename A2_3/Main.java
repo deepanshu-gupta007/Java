119a41d8e4a03927855b06c8d93d3a684304583f
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:18:56 +0530

    Add files via upload

--- A2_3/Main.java
+++ A2_3/Main.java
@@ -1,1 +1,30 @@
-
+class Table extends Thread{
+    synchronized void printTable(int n){
+        for(int i=1; i<=5; i++){
+            System.out.println(n*i);
+            try{
+                Thread.sleep(400);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+    Thread t1=new Thread(){
+        public void run(){
+            printTable(5);
+        }
+    };
+    Thread t2=new Thread(){
+        public void run(){
+            printTable(100);
+        }
+    };
+}
+public class Main {
+    public static void main(String[] args) {
+        Table obj=new Table();
+        obj.t1.start();
+        obj.t2.start();
+    }
+}
