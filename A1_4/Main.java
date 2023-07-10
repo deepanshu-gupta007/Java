9f6a8e731074bc17691334fc4531c72e46deda28
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:15:58 +0530

    Add files via upload

--- A1_4/Main.java
+++ A1_4/Main.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int x=sc.nextInt();
+        for(int i=1; i<=x; i++) {
+            for(int j=x; j>i; j--)
+                System.out.print(" ");
+            for(int k=1; k<=i; k++)
+                System.out.print(i+" ");
+            System.out.println();
+        }
+        sc.close();
+    }
+}
