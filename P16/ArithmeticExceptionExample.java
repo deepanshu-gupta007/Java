033b3fb1ee7c6753ccc9cc86d8e367d7ac6d617d
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:08:00 +0530

    Add files via upload

--- P16/ArithmeticExceptionExample.java
+++ P16/ArithmeticExceptionExample.java
@@ -1,1 +1,49 @@
-
+public class ArithmeticExceptionExample{
+    public static void main(String[] args){
+        int a=10;
+        int b=0;
+        try{
+            int result=a/b;
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        int c=Integer.MAX_VALUE;
+        int d=2;
+        try{
+            int result=c*d;
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        double x=10.0;
+        double y=0.0;
+        try{
+            double result=x/y;
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        int g=10;
+        int h=0;
+        try{
+            int result=g%h;
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        double i=2.0;
+        double j=-3.0;
+        try{
+            double result=Math.pow(i, j);
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
