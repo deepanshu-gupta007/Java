d425084bdf2d5da5c3e77f38f696c773655e5f5b
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:17:54 +0530

    Add files via upload

--- A2_1/Main.java
+++ A2_1/Main.java
@@ -1,1 +1,41 @@
-
+class LevelOneException extends Exception{
+    public LevelOneException(){
+        System.out.println("Level One Exception");
+    }
+}
+class LevelTwoException extends LevelOneException{
+    public LevelTwoException(){
+        System.out.println("Level Two Exception");
+    }
+}
+class LevelThreeException extends LevelTwoException{
+    public LevelThreeException(){
+        System.out.println("Level Three Exception");
+    }
+}
+class A{
+    public void method() throws LevelOneException{
+        throw new LevelOneException();
+    }
+}
+class B extends A{
+    public void method() throws LevelTwoException{
+        throw new LevelTwoException();
+    }
+}
+class C extends B{
+    public void method() throws LevelThreeException{
+        throw new LevelThreeException();
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        A obj=new C();
+        try{
+            obj.method();
+        } 
+        catch (LevelOneException e){
+            System.out.println("Caught Level One Exception");
+        }
+    }
+}
