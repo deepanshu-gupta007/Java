cb071d51d885ef753eaedb8959ccb9886dd9e9f1
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:14:35 +0530

    Add files via upload

--- A1_1/Main.java
+++ A1_1/Main.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name=sc.nextLine();
+        System.out.println("Hello "+name);
+        sc.close();
+    }
+}
