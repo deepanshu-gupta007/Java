6cf484c47dc052b8bbed52d2dd5a4305b15b406f
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:07:15 +0530

    Add files via upload

--- P15/TriangleArea.java
+++ P15/TriangleArea.java
@@ -1,1 +1,30 @@
-
+import java.util.Scanner;
+class Triangle {
+    private double base;
+    private double height;
+    public Triangle(double b, double h){
+        base=b;
+        height=h;
+    }
+    public double getArea(){
+        return 0.5*base*height;
+    }
+}
+public class TriangleArea {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double base, height;
+        try{
+            System.out.print("Enter the base of the triangle: ");
+            base=sc.nextDouble();
+            System.out.print("Enter the height of the triangle: ");
+            height=sc.nextDouble();
+            Triangle triangle=new Triangle(base, height);
+            double area=triangle.getArea();
+            System.out.println("The area of the triangle is "+area);
+        } 
+        catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
