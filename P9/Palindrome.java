121c6baa48c8799bcb00482828132c4b562caef3
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 21:59:57 +0530

    Add files via upload

--- P9/Palindrome.java
+++ P9/Palindrome.java
@@ -1,1 +1,11 @@
-
+public class Palindrome {
+    public static void main(String args[]) {
+        String rev="";
+        for(int i=args[0].length()-1; i>=0; i--)
+            rev=rev+args[0].charAt(i);
+        if (args[0].equals(rev))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
