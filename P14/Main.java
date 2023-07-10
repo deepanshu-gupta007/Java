6c90c5a07e420e1d9308e27c424a7a068c364eb6
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:04:27 +0530

    Add files via upload

--- P14/Main.java
+++ P14/Main.java
@@ -1,1 +1,17 @@
-
+import MyPackage.*;
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        setdata ob=new setdata();
+        ob.inputinfo();
+        a=ob.val1();
+        b=ob.val2();
+        add ob1=new add() {
+            public void sum() {
+                int res=a+b;
+                System.out.println("Sum = "+res);
+            }
+        };
+        ob1.sum();
+    }
+}
