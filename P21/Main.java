8c7ab56fe7e45f54cb1670750c609ad809b3a8c1
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:11:22 +0530

    Add files via upload

--- P21/Main.java
+++ P21/Main.java
@@ -1,1 +1,28 @@
-
+class Table{
+    synchronized void printTable(int n){
+        for(int i=1; i<=5; i++){
+            System.out.println(n*i);
+            try{
+                Thread.sleep(100);
+            }
+            catch(Exception e){}
+        }
+    }
+}
+public class Main extends Thread{
+    public static void main(String[] args) {
+        Table obj=new Table();
+        Thread t1=new Thread(){
+            public void run(){
+                obj.printTable(5);
+            }
+        };
+        Thread t2=new Thread(){
+            public void run(){
+                obj.printTable(100);
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+}
