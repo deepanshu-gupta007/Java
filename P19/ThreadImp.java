1fae13d4486274718af4b9dea62c9b318fa4f2b1
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:10:23 +0530

    Add files via upload

--- P19/ThreadImp.java
+++ P19/ThreadImp.java
@@ -1,1 +1,34 @@
-
+class X implements Runnable{
+    private String x;
+    public X(String x) {
+        this.x=x;
+    }
+    public void run() {
+        try{
+            System.out.println("Thread is complete "+x);
+            System.out.println("Thread is finished "+x);
+        }
+        catch(Exception e){
+            System.out.println("Exception occurs: "+e);
+        }
+    }
+}
+public class ThreadImp{
+    public static void main(String[] args){
+        Thread t1=new Thread(new X("Thread 1"));
+        Thread t2=new Thread(new X("Thread 2"));
+        Thread t3=new Thread(new X("Thread 3"));
+        t1.start();
+        t2.start();
+        t3.start();
+        try{
+            t1.join();
+            t2.join();
+            t3.join();
+        } 
+        catch (InterruptedException e){
+            System.out.println("Error: "+e);
+        }
+        System.out.println("All threads have finished.");
+    }
+}
