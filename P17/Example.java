1adc9cb35bd7a2a88d47f7dfbb6058579d3d6aaa
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:09:16 +0530

    Add files via upload

--- P17/Example.java
+++ P17/Example.java
@@ -1,1 +1,23 @@
-
+public class Example{
+    public static void main(String[] args){
+        int[] arr={1, 2, 3, 4, 5};
+        try{
+            int num=arr[-1];
+        } 
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        try{
+            int num=arr[10];
+        } 
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        try{
+            int num=arr[arr.length];
+        } 
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
