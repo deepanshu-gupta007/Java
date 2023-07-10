c35d5349f29185660e623db69cb67bab418a4076
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 22:16:54 +0530

    Add files via upload

--- A1_6/Main.java
+++ A1_6/Main.java
@@ -1,1 +1,10 @@
-
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {       
+        LocalDate d=LocalDate.of(2003, 10, 18);
+        String day=d.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        System.out.println("Day = "+day);
+    }
+}
