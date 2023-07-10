6582ba0f61058eb6c39a968d6f009d5c4f731943
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 21:56:57 +0530

    Add files via upload

--- P4/Age.java
+++ P4/Age.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+import java.time.*;
+public class Age {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter DOB in YYYY-MM-DD format: ");
+        String x=sc.nextLine();
+        LocalDate dob=LocalDate.parse(x);
+        LocalDate td=LocalDate.now();
+        int age=Period.between(dob, td).getYears();
+        System.out.println("Age = "+age);
+        sc.close();
+    }
+}
