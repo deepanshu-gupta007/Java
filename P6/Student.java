91f6aa44864d436b89e10e58224040e2e80359dc
Deepanshu Gupta <99672453+deepanshu-gupta007@users.noreply.github.com>
2023-07-10 21:58:07 +0530

    Add files via upload

--- P6/Student.java
+++ P6/Student.java
@@ -1,1 +1,12 @@
-
+public class Student {
+    public static void main(String args[]) {
+        String name=args[0];
+        String address=args[1];
+        int rollno=Integer.parseInt(args[2]);
+        long phno=Long.parseLong(args[3]);
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+address);
+        System.out.println("Phone No.: "+phno);
+        System.out.println("Roll No.: "+rollno);
+    }
+}
